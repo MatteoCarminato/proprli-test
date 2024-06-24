272c5b1bacd08628d3164b0d376b4e7b72a9ed11
Matteo Carminato <mcarminato95@gmail.com>
2024-06-24 09:46:24 -0300

    feat(seeds): add seeds and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Construction;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory(3)->create();
+         Construction::factory(3)->create();
+         Task::factory(3)->create();
+         Comment::factory(3)->create();
     }
 }
